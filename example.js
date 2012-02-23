df3fe068453671e08f5c4a46cc4000274998c721
martinrusev <martinrusev@zoho.com>
2012-02-23 19:26:51 +0000

    Simplified configuration, doesn't rely on /etc/amon.conf

--- example.js
+++ example.js
@@ -1,31 +1,39 @@
 var amon = require('amon').Amon;
+amon.app_key = 'test'
 
-amon.log('node.js unset message')
-amon.log('node.js debug message', ['Martin' ,'debug', 'development'])
-amon.log('node.js warning message', 'warning')
-amon.log('node.js info message', 'info')
+//amon.log('node.js unset message')
+//amon.log('node.js debug message', ['Martin' ,'debug', 'development'])
+//amon.log('node.js warning message', 'warning')
+//amon.log('node.js info message', 'info')
 
-json={"widget": {"debug": "on", "text": {"vOffset": 100, "style": "bold", "name": "text1", "hOffset": 250, "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;", "data": "Click Here", "alignment": "center", "size": 36}, "window": {"width": 500, "height": 500, "name": "main_window", "title": "Sample Konfabulator Widget"}, "image": {"vOffset": 250, "src": "Images/Sun.png", "alignment": "center", "name": "sun1", "hOffset": 250}}}
+//json={"widget": {"debug": "on", "text": {"vOffset": 100, "style": "bold", "name": "text1", "hOffset": 250, "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;", "data": "Click Here", "alignment": "center", "size": 36}, "window": {"width": 500, "height": 500, "name": "main_window", "title": "Sample Konfabulator Widget"}, "image": {"vOffset": 250, "src": "Images/Sun.png", "alignment": "center", "name": "sun1", "hOffset": 250}}}
 
-amon.log(json)
-amon.log(json,'warning')
+//amon.log(json)
+//amon.log(json,'warning')
 
 
-image_json={
+image_dictionary={
   "Image": {
-    "Width":800,
-    "Height":600,
-    "Title":"View from 15th Floor",
-    "Thumbnail":
-    {
-      "Url":"http:\/\/scd.mm-b1.yimg.com\/image\/481989943",
-      "Height": 125,
-      "Width": "100"
-    }
+	"Width":800,
+	"Height":600,
+	"Title":"View from 15th Floor",
+	"Url": "http://static.amon.cx/3490fdd.png",
+	"Thumbnail":
+	{
+	  "Url":"http://static.amon.cx/1232323.png",
+	  "Height": 125,
+	  "Width": 100
+	}
   }
 }
 
-amon.log(image_json, ['Martin','images', 'debug'])
+amon.log(image_dictionary, ['Martin','images', 'debug'])
+
+
+//amon.log('log message', 'Martin')
+
+//amon.log('log message', ['Martin', 'user_data', 'debug'])
+//amon.log('log message', ['John', 'benchmark'])
 
 process.addListener('uncaughtException', function(err) {
 	amon.handle(err);

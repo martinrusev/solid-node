39081a5a95d741597bec3f9cdf8ad16a9e8b81fa
Michael Heap <m@michaelaheap.com>
2012-02-26 20:45:36 +0000

    Library fixed. Tests updated

--- test/amon-test.js
+++ test/amon-test.js
@@ -6,25 +6,26 @@ var Amon = require('../lib/amon').Amon;
 
 
 exports.testAmonPort = function(test){
-    test.equal(Amon.port, 2464);
-    test.done();
+  test.equal(Amon.port, 2464);
+  test.done();
 };
 
 exports.testAmonHost = function(test){
-    test.equal(Amon.host, '127.0.0.1');
-    test.done();
+  test.equal(Amon.host, '127.0.0.1');
+  test.done();
 };
 
 exports.testExceptionHandler = function(test){
+  var exception_label = "Exception here";
   try {
-    throw new Error("Exception here");
-  } catch(error) {
+    throw new Error(exception_label);
+  } 
+  catch(error) {
     var doc = Amon.exception_data_to_json(error);
     var json = JSON.parse(doc);
-	console.log(json);
 
     test.equal(json.message, "Exception here");
-    test.equal(json.exception_class, "node");
+    test.equal(json.exception_class, "Error: " + exception_label);
     test.done();
   }
 };

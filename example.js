64299e3c5ce63d8dbc44b1d5e99c839d27a8ddde
martinrusev <martinrusev@zoho.com>
2012-03-28 12:00:38 +0100

    ZeroMQ support

--- example.js
+++ example.js
@@ -1,16 +1,8 @@
-var amon = require('amon').Amon;
-amon.app_key = 'test'
-
-//amon.log('node.js unset message')
-//amon.log('node.js debug message', ['Martin' ,'debug', 'development'])
-//amon.log('node.js warning message', 'warning')
-//amon.log('node.js info message', 'info')
-
-//json={"widget": {"debug": "on", "text": {"vOffset": 100, "style": "bold", "name": "text1", "hOffset": 250, "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;", "data": "Click Here", "alignment": "center", "size": 36}, "window": {"width": 500, "height": 500, "name": "main_window", "title": "Sample Konfabulator Widget"}, "image": {"vOffset": 250, "src": "Images/Sun.png", "alignment": "center", "name": "sun1", "hOffset": 250}}}
-
-//amon.log(json)
-//amon.log(json,'warning')
-
+var amon = require('./lib/amon.js').Amon;
+amon.protocol = 'zeromq'
+amon.host = 'localhost'
+amon.port = 5464
+//amon.app_key = 'test'
 
 image_dictionary={
   "Image": {
@@ -30,7 +22,7 @@ image_dictionary={
 amon.log(image_dictionary, ['Martin','images', 'debug'])
 
 
-//amon.log('log message', 'Martin')
+amon.log('zeromq node js log message', 'Martin')
 
 //amon.log('log message', ['Martin', 'user_data', 'debug'])
 //amon.log('log message', ['John', 'benchmark'])
@@ -39,12 +31,12 @@ process.addListener('uncaughtException', function(err) {
 	amon.handle(err);
 });
 
-//try {
-  //throw new Error("Test Error 1");
-//} catch(error) {
-  //console.log("Error occurred ", error.message);
-  //Amon.handle(error);
-//}
+try {
+  throw new Error("Test Error 1");
+} catch(error) {
+  console.log("Error occurred ", error.message);
+  amon.handle(error);
+}
 
 
 throw new Error("Test Error 2");
